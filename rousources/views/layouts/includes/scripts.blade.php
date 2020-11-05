546a2bc9de505a88e625c8505c3fcd46979b0fdc
dew9163 <dew9163.flybook@gmail.com>
2020-11-05 14:55:01 +0900

    update resources

--- rousources/views/layouts/includes/scripts.blade.php
+++ rousources/views/layouts/includes/scripts.blade.php
@@ -1,5 +1,5 @@
 <script src="{{ asset('js/admin_app.js') }}"></script>
-{{--<script src="{{ asset('js/fb.js') }}" ></script>--}}
+{{--<script src="{{ asset('js/helper.js') }}" ></script>--}}
 
 <!-- Bootstrap core JavaScript-->
 {{--<script src="{{ asset('resources/template/vendor/jquery/jquery.min.js')}}"></script>--}}
