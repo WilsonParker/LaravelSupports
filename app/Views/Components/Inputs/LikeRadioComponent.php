d5e2cbab6dfa9289fd54839dde9a3dd21a0bbf47
dew9163 <dew9163.flybook@gmail.com>
2020-11-20 18:19:40 +0900

    update Component

--- app/Views/Components/Inputs/LikeRadioComponent.php
+++ app/Views/Components/Inputs/LikeRadioComponent.php
@@ -11,6 +11,7 @@ class LikeRadioComponent extends BaseComponent
     const KEY_VALUE = 'value';
     const KEY_CHECKED = 'checked';
     const KEY_NON_CHECKED = 'non_checked';
+    const KEY_IS_CHECKED = 'is_checked';
 
     protected string $view = 'input.like_radio_component';
 
@@ -33,31 +34,33 @@ class LikeRadioComponent extends BaseComponent
      * view 를 꾸미기 위한 data 생성
      *
      * @param array $arr
-     * @param $text
-     * @param $value
-     * @param $checked
-     * @param $nonChecked
+     * @param string $text
+     * @param string $value
+     * @param string $checked
+     * @param string $nonChecked
+     * @param bool $isChecked
      * @return array
      * @author  dew9163
      * @added   2020/11/05
      * @updated 2020/11/05
      */
-    public static function buildDataWithArray(array &$arr, $text, $value, $checked, $nonChecked): array
+    public static function buildDataWithArray(array &$arr, string $text, string $value, string $checked, string $nonChecked, bool $isChecked = false): array
     {
-        $result = self::buildData($text, $value, $checked, $nonChecked);
+        $result = self::buildData($text, $value, $checked, $nonChecked, $isChecked);
         if (isset($arr)) {
             array_push($arr, $result);
         }
         return $result;
     }
 
-    public static function buildData($text, $value, $checked, $nonChecked)
+    public static function buildData(string $text, string $value, string $checked, string $nonChecked, bool $isChecked = false): array
     {
         return [
             self::KEY_TEXT => $text,
             self::KEY_VALUE => $value,
             self::KEY_CHECKED => $checked,
             self::KEY_NON_CHECKED => $nonChecked,
+            self::KEY_IS_CHECKED => $isChecked,
         ];
     }
 }
--- rousources/views/layouts/components/input/like_radio_component.blade.php
+++ rousources/views/layouts/components/input/like_radio_component.blade.php
@@ -1,3 +1,4 @@
+@inject('component','LaravelSupports\Views\Components\Inputs\LikeRadioComponent')
 <div class="form-group">
     @isset($title)
         {{ $title }}
@@ -5,17 +6,20 @@
 
     @foreach($items as $values)
         <div class="form-check form-check-inline">
-            <input class="form-check-input input_radio" type="radio" id="input_radio_{{ $name }}_{{ $values['value'] }}"
+            <input class="form-check-input input_radio" type="radio" id="input_radio_{{ $name }}_{{ $values[$component::KEY_VALUE] }}"
                    name="{{ $name }}"
-                   value="{{ $values['value'] }}"
+                   value="{{ $values[$component::KEY_VALUE] }}"
+                   @if($values[$component::KEY_IS_CHECKED])
+                   checked
+                   @endif
                    style="display: none">
             <i class="" aria-hidden="true"
                style="margin-right: 5px"
-               data-checked="fa {{ $values['checked'] }}"
-               data-non-checked="fa {{ $values['non_checked'] }}"
+               data-checked="fa {{ $values[$component::KEY_CHECKED] }}"
+               data-non-checked="fa {{ $values[$component::KEY_NON_CHECKED] }}"
             ></i>
-            <label class="form-check-label" for="input_radio_{{ $name }}_{{ $values['value'] }}">
-                {{ $values['text'] }}
+            <label class="form-check-label" for="input_radio_{{ $name }}_{{ $values[$component::KEY_VALUE] }}">
+                {{ $values[$component::KEY_TEXT] }}
             </label>
         </div>
     @endforeach
