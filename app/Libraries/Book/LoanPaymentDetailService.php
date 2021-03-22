358dc15090ec60dce6338ef45e3ec9e5d36ebf1f
seul <dew89@flybook.kr>
2021-03-22 11:37:58 +0900

    update loan return date

--- app/Libraries/Book/LoanPaymentDetailService.php
+++ app/Libraries/Book/LoanPaymentDetailService.php
@@ -144,7 +144,7 @@ class LoanPaymentDetailService
                     if ($returnBook->count()) {
                         $countDoesntHavePickupDate = $returnBook->filter(function ($item) {
                             $history = $item->history;
-                            return $history->return_date == null;
+                            return $history->pickup_date == null;
                         })->count();
                         if ($countDoesntHavePickupDate != count($returnBook)) {
                             if ($countDoesntHavePickupDate > 0) {
