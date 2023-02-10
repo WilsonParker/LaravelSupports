5c0b7895c867e6512e5877abcaf18800e6f66d96
Wilson <zerad3208@gmail.com>
2023-02-10 17:30:44 +0900

    add foreignCode

--- app/Libraries/Supports/Databases/Migrations/BaseMigration.php
+++ app/Libraries/Supports/Databases/Migrations/BaseMigration.php
@@ -3,13 +3,17 @@
 
 namespace LaravelSupports\Libraries\Supports\Databases\Migrations;
 
+use Illuminate\Database\Eloquent\Concerns\HasRelationships;
 use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Schema\ForeignKeyDefinition;
 use Illuminate\Support\Facades\Schema;
 
 // php artisan migrate --path='./database/migrations/2020_04_26_033358_create_member_overdue_information.php'
 abstract class BaseMigration extends Migration
 {
+    use HasRelationships;
+
     protected string $table;
     protected bool $needDrop = true;
     protected bool $timestamp = true;
@@ -27,7 +31,7 @@ abstract class BaseMigration extends Migration
         });
     }
 
-    protected function defaultSet(Blueprint $table)
+    protected function defaultSet(Blueprint $table): void
     {
 //        $table->engine = 'InnoDB';
 //        $table->charset = 'utf8mb4';
@@ -36,7 +40,7 @@ abstract class BaseMigration extends Migration
 //        $table->collation = 'utf8_general_ci';
     }
 
-    protected function defaultTimestampTemplate(Blueprint $table)
+    protected function defaultTimestampTemplate(Blueprint $table): void
     {
         if ($this->timestamp) {
             $table->timestamp('created_at')->useCurrent();
@@ -53,9 +57,30 @@ abstract class BaseMigration extends Migration
      * @param Blueprint $table
      * @return void
      */
-    protected function defaultDownTemplate(Blueprint $table)
-    {
+    protected function defaultDownTemplate(Blueprint $table): void {}
 
+    /**
+     * Foreign for table where pk is code
+     *
+     * @param \Illuminate\Database\Schema\Blueprint $bluePrint
+     * @param array|string                          $columns
+     * @param string                                $table
+     * @param array|string                          $references
+     * @return \Illuminate\Database\Schema\ForeignKeyDefinition
+     * @author  WilsonParker
+     * @added   2023/02/10
+     * @updated 2023/02/10
+     */
+    protected function foreignCode(
+        Blueprint    $bluePrint,
+        array|string $columns,
+        string       $table,
+        array|string $references = 'code'
+    ): ForeignKeyDefinition {
+        /**
+         * @var \Illuminate\Database\Eloquent\Model $referTable
+         */
+        $referTable = new $table;
+        return $bluePrint->foreign($columns)->references($references)->on($referTable->getTable());
     }
-
 }
