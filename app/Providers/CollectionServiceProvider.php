25ccaf44ecb69c74e3f7f31b034b8c741cce1dc7
Wilson <zerad3208@gmail.com>
2023-03-02 01:10:25 +0900

    Update CollectionServiceProvider.php
    exclude
    - add parameter type
    - update $item type
    excludeKeys
    - add parameter type

--- app/Providers/CollectionServiceProvider.php
+++ app/Providers/CollectionServiceProvider.php
@@ -12,20 +12,22 @@ class CollectionServiceProvider extends ServiceProvider
     {
         /**
          * Model 으로 이루어진 collection 에서 $needle 과 곂치는 부분이 있는지 확인 합니다
-         *
          * collection 일 경우 $prop 또는 primaryKey 에 적합한 값이 있는지 확인 합니다.
          * array 일 경우 collection 으로 변환 후 재실행 합니다.
          * callable 일 경우 $needle callback 에 적합한 값이 있는지 확인 합니다.
          * Model 또는 string 일 경우 primaryKey 또는 $prop 에 적합한 값이 있는지 확인 합니다.
          *
          * @param Collection | array | Model $needle
-         * @param string $pros
+         * @param string                     $pros
          * @return bool
          * @author  WilsonParker
          * @added   2020/11/11
          * @updated 2021/10/12
          */
-        Collection::macro('exists', function (Collection|array|callable|Model|string $needle, string $prop = null): bool {
+        Collection::macro('exists', function (
+            Collection|array|callable|Model|string $needle,
+            string                                 $prop = null
+        ): bool {
             $getPropValue = function (Model $item) use ($prop) {
                 return isset($prop) ? $item->{$prop} : $item->getPrimaryValue();
             };
@@ -90,7 +92,7 @@ class CollectionServiceProvider extends ServiceProvider
          * $callback 에 해당하는 내용이 존재하는지 확인 합니다
          *
          * @param Collection | array | Model $needle
-         * @param $callback
+         * @param                            $callback
          * function($item, $needle)
          * @return bool
          * @author  WilsonParker
@@ -122,7 +124,7 @@ class CollectionServiceProvider extends ServiceProvider
          * $collection 에 단일 item 을 넘길 수 있습니다
          *
          * @param Collection | array | Model $collection
-         * @param string $prop
+         * @param string                     $prop
          * @return Collection
          * @author  WilsonParker
          * @added   2020/11/12
@@ -160,31 +162,34 @@ class CollectionServiceProvider extends ServiceProvider
         /**
          * $prop 으로 비교하여 $collection 에 해당하는 item 들을 제외시킨 Collection 을 제공 합니다
          *
-         * @param Collection $collection
-         * @param string $prop
+         * @param Collection|array $collection
+         * @param string           $prop
          * @return Collection
          * @author  WilsonParker
          * @added   2020/11/11
          * @updated 2020/11/11
+         * @updated 2023/03/02
          */
-        Collection::macro('exclude', function (Collection $collection, string $prop = 'id'): Collection {
+        Collection::macro('exclude', function (Collection|array $collection, string $prop = 'id'): Collection {
+            $collection = collect($collection);
             return $this->filter(function ($item) use ($collection, $prop) {
-                return !$collection->exists($item, $prop);
+                return !$collection->exists($item, is_object($item) ? $prop : null);
             });
         });
 
         /**
          * key 값이 $arr 에 포함되지 않는 데이터를 제공 합니다
          *
-         * @param array $arr
+         * @param Collection|array $data
          * @return Collection
          * @author  WilsonParker
          * @added   2020/12/08
          * @updated 2020/12/08
+         * @updated 2023/03/02
          */
-        Collection::macro('excludeKeys', function (array $arr): Collection {
-            return $this->filter(function ($item, $key) use ($arr) {
-                return !in_array($key, $arr);
+        Collection::macro('excludeKeys', function (Collection|array $collection): Collection {
+            return $this->filter(function ($item, $key) use ($collection) {
+                return !in_array($key, $collection);
             });
         });
 
@@ -212,20 +217,22 @@ class CollectionServiceProvider extends ServiceProvider
 
         /**
          * Model 으로 이루어진 collection 에서 $needle 에 해당하는 값을 제공합니다
-         *
          * collection 일 경우 $prop 또는 primaryKey 에 적합한 값이 있는지 확인 합니다.
          * array 일 경우 collection 으로 변환 후 재실행 합니다.
          * callable 일 경우 $needle callback 에 적합한 값이 있는지 확인 합니다.
          * Model 또는 string 일 경우 primaryKey 또는 $prop 에 적합한 값이 있는지 확인 합니다.
          *
          * @param Collection | array | Model $needle
-         * @param string $pros
+         * @param string                     $pros
          * @return Model
          * @author  WilsonParker
          * @added   2021/11/18
          * @updated 2021/11/18
          */
-        Collection::macro('getFirst', function (Collection|array|callable|Model|string $needle, string $prop = null): ?Model {
+        Collection::macro('getFirst', function (
+            Collection|array|callable|Model|string $needle,
+            string                                 $prop = null
+        ): ?Model {
             $getPropValue = function (Model $item) use ($prop) {
                 return isset($prop) ? $item->{$prop} : $item->getPrimaryValue();
             };
