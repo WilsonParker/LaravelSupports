50f1031ec74d0dd5111cfbac66ab9c387ffb19ab
allen <allen@mirrorboard.com>
2024-02-16 19:00:33 +0900

    update resources

---
+++ Resources/src/BaseCodeResources.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace LaravelSupports\Resources;
+
+use Illuminate\Http\Request;
+
+/**
+ * @OA\Schema(
+ *      @OA\Property(property="code", type="string", description="primary key", readOnly="true", example="code"),
+ * )
+ * Class BaseCodeResources
+ *
+ * @package App\Resources
+ */
+abstract class BaseCodeResources extends BaseResources
+{
+    protected bool $showAppendFields = true;
+
+    function appendsFields(Request $request): array
+    {
+        return $this->showAppendFields ? ['id' => $this->code] : [];
+    }
+
+}
